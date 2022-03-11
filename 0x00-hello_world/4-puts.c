20fafc68fc9879fec5debba5f53cf16a29893f31
dawitdethio <dawitdereje07@gmail.com>
2022-03-11 10:28:21 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+*main -entry point
  *
- * Return: 0 after printing the function
- * /
- int main(void)
- {
- puts("\"programming is like bulding a multilingual puzzle");
- return (0);
- }
+ *return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
