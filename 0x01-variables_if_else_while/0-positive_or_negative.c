378c6ffb97bc8afc0f654b4769b0a026213e02e8
dawitdethio <dawitdereje07@gmail.com>
2022-03-13 06:28:48 -0700

    postive is butter than anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,16 @@
 
 /**
  *main - Entry point
+ *
  *Return: Always 0 (Success/correct)
  */
 
 int main(void)
 {
 int n;
+
 srand(time(0));
-n = rand() -RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
