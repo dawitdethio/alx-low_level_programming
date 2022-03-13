9ae602ffe584d0d29857d42c87baad3a90051929
dawitdethio <dawitdereje07@gmail.com>
2022-03-13 12:28:37 -0700

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - Entry point 
+ * main - Entry point
  *
- *Return: Always 0 (success/correct)
-*/
+ * Return: Always 0 (success/correct)
+ */
 int main(void)
 
 {
@@ -11,15 +11,16 @@ char base_16;
 
 for (base_16 = 48; base_16 <= 57; base_16++)
 {
-putchar(base_16);
+	putchar(base_16);
 }
 
-for (base_16 = 97; base_16 <= 102; base_16++)
+for (base_16 = 97; base_16 < 102; base_16++)
 {
-putchar(base_16);
+	putchar(base_16);
 }
 
-putchar ('\n');
+putchar ('\n')
 
 return (0);
+
 }
