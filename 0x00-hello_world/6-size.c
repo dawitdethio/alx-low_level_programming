9b76863b30c5fd522fac0807b5c6ba1e7ffbe8e5
dawitdethio <dawitdereje07@gmail.com>
2022-03-14 09:20:18 -0700

    territory dont make nations

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /**
+ *
  * main - Entry point
  *
  * Return: Always 0 (success)
@@ -11,5 +12,7 @@ printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("size of a float: %d byte(s)\n", sizeof(float));
+
 return (0);
+
 }
