e3f7074188686a812daa5fc64c1b249dd0c22a64
dawitdethio <dawitdereje07@gmail.com>
2022-03-16 13:11:11 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 			printf("%d", i);
 			if (i != 98)
 			{
-				printf(", ");
+				printf(",");
 			}
 		}
 	}
@@ -27,7 +27,7 @@ void print_to_98(int n)
 			printf("%d", i);
 			if (i != 98)
 			{
-				printf(", ");
+				printf(",");
 			}
 		}
 	}
