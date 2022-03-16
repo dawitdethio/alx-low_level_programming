71e9d2e5a1e5c616471b243aeb624f81b1f01a9d
dawitdethio <dawitdereje07@gmail.com>
2022-03-16 12:17:24 -0700

    jack b

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute the day 00:00 to 23:59
+ * jack_bauer - prints every minute of the day 00:00 to 23:59
  * @void - no argument
  */
 void jack_bauer(void)
 {
 	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
