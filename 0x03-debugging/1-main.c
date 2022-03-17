e2c26c763a5b32c707cd3cba59a472d7a1708511
dawitdethio <dawitdereje07@gmail.com>
2022-03-17 10:09:46 -0700

    main secnd

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,19 +6,20 @@
  */
 int main(void)
 {
-	int i;
-printf("infinite loop incoming :(\n");
+int i;
+
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
 /*
  * while (i < 10)
  * {
- * infinite loop - no increase of variable
+ * Infinite loop - No increase of variable
  * putchar(i);
 }
 */
 
-printf("infinite loop avoided! \\0/\n");
+printf("Infinite loop avoided! \\0/\n");
 return (0);
-}
+i}
