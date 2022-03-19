fc721fd6199c258a21a3e8674eb7693e25181ec4
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-03-19 11:01:40 +0300

    more num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,30 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14,
- * followed by a new line.
- *
- * Return: void
- *
- */
-
+*more_numbers - prints from 0 to 14 ten times.
+*
+* Return: the output
+*/
 void more_numbers(void)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 15; j++)
-		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-
-			_putchar(j % 10 + '0');
-
-
-		}
-
-		_putchar('\n');
-
-	}
-
+for (j = 0; j < 10 ; ++j)
+{
+for (i = 0; i < 15; ++i)
+{
+if (i > 9)
+{
+_putchar(i / 10 + '0');
+}
+_putchar(i % 10 + '0');
+}
+_putchar('\n');
+}
 }
