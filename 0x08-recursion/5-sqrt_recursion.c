3eda74edcda72712e6f403beec7b824db01c980a
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-03-30 19:17:38 +0300

    five sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+* con - function that returns the natural square root of a number.
+* @i: integer.
+* @n: integer.
+* Return: ..
+*/
+
+int con(long int n, long int i)
+{
+	if (i * i > n)
+	{
+	return (-1);
+	}
+		if (i * i == n)
+		{
+		return (i);
+		}
+return (con(n, i + 1));
+}
+
+
+
+
+/**
+* _sqrt_recursion - function that returns the natural square root of a number.
+* @n: integer.
+* Return: ..
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+	return (0);
+	}
+		if (n < 0)
+		{
+		return (-1);
+		}
+return (con(n, 1));
+}
