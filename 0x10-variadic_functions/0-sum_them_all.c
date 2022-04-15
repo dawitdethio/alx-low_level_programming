36907bf29bb0226e86ebeb9d7de39c073d4ef9f3
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-04-15 21:40:54 +0300

    sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,24 @@
-#ifndef VARIADIC_HEADER
-#define VARIADIC_HEADER
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-#endif
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+* sum_them_all - function that returns the sum of all its parameters.
+* @n: integer.
+* Return: sum of arguments.
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i = 0, sum = 0;
+va_list list;
+
+	if (n == 0)
+	return (0);
+va_start(list, n);
+		while (i < n)
+		{
+		sum += va_arg(list, unsigned int);
+		i++;
+		}
+va_end(list);
+return (sum);
+}
