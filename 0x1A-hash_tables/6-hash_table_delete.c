9e23cf738d79fe4a3e426c50dd43f9ae8392c4a4
dawitdethio <99313483+dawitdethio@users.noreply.github.com>
2022-07-05 14:08:14 +0300

    hash tbl del

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,36 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_delete - function that deletes a hash table
-* @ht: hash table
-**/
+ * free_list - frees a linked list
+ * @head: hash_node_t list to be freed
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->key);
+		free(head->value);
+		free(head);
+		head = temp;
+	}
+}
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to be deleted
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned int i = 0;
-	hash_node_t *tmp = NULL;
+	unsigned long int i;
 
 	if (!ht)
 		return;
-	while (i < ht->size)
-	{
-		if (ht->array[i])
-		{
-			while (ht->array[i])
-			{
-				tmp = ht->array[i];
-				free(ht->array[i]->key);
-				free(ht->array[i]->value);
-				ht->array[i] = ht->array[i]->next;
-				free(tmp);
-			}
-		}
-		i++;
-	}
+
+	for (i = 0; i < ht->size; i++)
+		free_list(ht->array[i]);
 	free(ht->array);
 	free(ht);
 }
